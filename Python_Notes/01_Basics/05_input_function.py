74160b18e8f78e9b58f8706ae3a83ba0e8cba8a7
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-01-24 10:56:59 +0530

    Update 05_input_function.py

--- Python_Notes/01_Basics/05_input_function.py
+++ Python_Notes/01_Basics/05_input_function.py
@@ -1,1 +1,17 @@
+# input() function in Python is used to take input from the user (keyboard)
+# input() always returns a string(str).
 
+name = input("Enter your name: ")
+print(name)
+print(type(name))  # string
+
+# Taking integer as an input
+num1 = int(input("Enter First Number: "))    # First Number = 5
+num2 = int(input("Enter Second Number: "))   # Second Number = 2
+sum = num1 + sum2
+print("Sum of numbers: ",sum)  # Sum = 7
+
+# Taking float as an input
+num3 = float(input("Enter First number: "))    # First Number = 5.2
+num4 = float(input("Enter Second Number: "))   # Second Number = 6.4
+print("Sum of numbers: ",num3 + num4)  # Sum = 11.6
