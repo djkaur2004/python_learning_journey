227bb070f97bd9e05f671d09d7a79b7ae93d1aa5
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-01-25 12:06:39 +0530

    Update 01_lists_intro.py

--- Python_Notes/02_List/01_lists_intro.py
+++ Python_Notes/02_List/01_lists_intro.py
@@ -1,1 +1,24 @@
+# List is an ordered collection of data with elements separated by a comma and enclosed within a square bracket.
+# List are mutable and can be modified after creation.
 
+# Creating a list
+list1 = [1,2,3,4,5]
+print(list1)  # [1,2,3,4,5]
+print(type(list1))
+
+# List can store different datatypes
+list2 = [23, 8.9, "English", True]
+# changing value of an element in the list (mutable)
+list2[63] = 96
+print(list2)  # [96, 8.9, "English", True]
+
+# print elements using index
+fruits = ["apple", "orange", "grapes", "pear", "banana"]
+print(fruit[0])  # apple
+print(fruit[1])  # orange
+print(fruit[2])  # grapes
+
+# List Slicing
+# List slicing means taking a part (slice) of a list using index range.
+list3 = ["sam", "rohan", "sachin", 98, 78.9, 88]
+print("Slicing of list ",c[0:4])  # ["sam", "rohan", "sachin", 98]
