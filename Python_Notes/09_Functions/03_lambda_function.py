61d4ccbb6321f984c6399248905a469b4a57af1a
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-02-05 18:03:07 +0530

    Update 03_lambda_function.py

--- Python_Notes/09_Functions/03_lambda_function.py
+++ Python_Notes/09_Functions/03_lambda_function.py
@@ -1,1 +1,24 @@
+# Lambda Function
+# A lambda function is a small, anonymous function (a function without a name) written in one single line.
 
+# Syntax
+# lambda arguments : expression
+
+# Example 1: Comparison between Normal Function and Lambda Function
+# Normal Function
+def func_without_lambda(x):
+    return x*2
+print(func_without_lambda(20))  # 40
+
+# Lambda Function
+equiv = lambda x:x*2
+print(equiv(20))  # 40
+# much smaller than normal function
+
+# Example 2: Square of a Number
+square = lambda x: x * x
+print(square(5))  # 25
+
+# Example 3: Lmabda with multiple arguments
+equiv = lambda x,y:x*y*2
+print(equiv(20,40))  # 1600
